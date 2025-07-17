e7b3eea0e7a525cc432e86d68fa8d59c514b6136
Răzvan-Ștefan Florea <razvan.florea@stud.ubbcluj.ro>
2025-07-18 02:45:13 +0300

    Initial commit

--- BrainSwap_backend/src/main/java/com/FoRS/BrainSwap_backend/BrainSwapApplication.java
+++ BrainSwap_backend/src/main/java/com/FoRS/BrainSwap_backend/BrainSwapApplication.java
@@ -1,5 +1,6 @@
 package com.FoRS.BrainSwap_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BrainSwapApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
 		SpringApplication.run(BrainSwapApplication.class, args);
 	}
 
